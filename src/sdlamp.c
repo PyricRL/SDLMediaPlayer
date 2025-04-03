2ed96a615a6a2669617d98f9f201cafb072f914b
PyricYT <ljcosa@icloud.com>
2025-04-02 20:06:00 -0500

    More stuff

--- src/sdlamp.c
+++ src/sdlamp.c
@@ -28,10 +28,45 @@ static void panic_and_abort(const char *title, const char *text) {
     exit(1);
 }
 
+static float volume_slider_value = 1.0f;
+
+static Uint8 *wavbuf = NULL;
+static Uint32 wavlen = 0;
+static SDL_AudioSpec wavspec;
+static SDL_AudioStream *stream = NULL;
+
+static SDL_bool open_new_audio_file(const char *fname) {
+    SDL_FreeAudioStream(stream);
+    stream = NULL;
+    SDL_FreeWAV(wavbuf);
+    wavbuf = NULL;
+    wavlen = 0;
+
+    if (SDL_LoadWAV(fname, &wavspec, &wavbuf, &wavlen) == NULL) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file!", SDL_GetError(), window);
+        return SDL_FALSE;
+    }
+
+    stream = SDL_NewAudioStream(wavspec.format, wavspec.channels, wavspec.freq, AUDIO_F32, 2, 48000);
+    if (!stream) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't create audio stream!", SDL_GetError(), window);
+        SDL_FreeWAV(wavbuf);
+        wavbuf = NULL;
+        wavlen = 0;
+        return SDL_FALSE;
+    }
+
+    if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
+        panic_and_abort("Audio stream put failed", SDL_GetError());
+    }
+
+    SDL_AudioStreamFlush(stream);
+
+    return SDL_TRUE;
+}
+
 int main(int argc, char **argv) {
-    Uint8 *wavbuf = NULL;
-    Uint32 wavlen = 0;
-    SDL_AudioSpec wavspec;
+    SDL_AudioSpec desired;
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
         panic_and_abort("SDL_Init failed", SDL_GetError());
@@ -47,11 +82,26 @@ int main(int argc, char **argv) {
         panic_and_abort("SDL_CreateRenderer failed", SDL_GetError());
     }
 
-    SDL_EventState(SDL_DROPFILE, SDL_ENABLE); // tell SDL we want this event that is disabled by default
+    SDL_zero(desired);
+    desired.freq = 48000;
+    desired.format = AUDIO_F32;
+    desired.channels = 2;
+    desired.samples = 4096;
+    desired.callback = NULL;
+
+    audio_device = SDL_OpenAudioDevice(NULL, 0, &desired, NULL, 0);
+    if (audio_device == 0) {
+        panic_and_abort("Couldn't open audio device!", SDL_GetError());
+    }
+
+    SDL_EventState(SDL_DROPFILE, SDL_ENABLE);   // tell SDL we want this event that is disabled by default
+
+    open_new_audio_file("music.wav");
 
     SDL_bool paused = SDL_TRUE;
     const SDL_Rect rewind_rect = {100, 100, 100, 100};
     const SDL_Rect pause_rect = {400, 100, 100, 100};
+    const SDL_Rect volume_rect = {(640-500) / 2, 400, 500, 20};
 
     int green = 0;
     SDL_bool keep_going = SDL_TRUE;
@@ -59,63 +109,57 @@ int main(int argc, char **argv) {
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
-                case SDL_QUIT:
+                case SDL_QUIT: {
                     keep_going = SDL_FALSE;
                     break;
+                }
                 case SDL_MOUSEBUTTONDOWN: {
                     const SDL_Point pt = {e.button.x, e.button.y};
-                    if (SDL_PointInRect(&pt, &rewind_rect)) {
-                        if (audio_device) {
-                            SDL_ClearQueuedAudio(audio_device);
-                            SDL_QueueAudio(audio_device, wavbuf, wavlen);
+                    if (SDL_PointInRect(&pt, &rewind_rect)) {       // pressed the rewind button
+                        SDL_ClearQueuedAudio(audio_device);
+                        SDL_AudioStreamClear(stream);
+                        if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
+                            panic_and_abort("Audio stream put failed", SDL_GetError());
                         }
-                    } else if (SDL_PointInRect(&pt, &pause_rect)) {
+                        SDL_AudioStreamFlush(stream);
+                    } else if (SDL_PointInRect(&pt, &pause_rect)) { // pressed the pause button
+                        paused = paused ?  SDL_FALSE : SDL_TRUE;
                         if (audio_device) {
-                            paused = paused ?  SDL_FALSE : SDL_TRUE;
                             SDL_PauseAudioDevice(audio_device, paused);
                         }
+                    }
+                    break;
+                }
+                case SDL_MOUSEMOTION: {
+                    const SDL_Point pt = { e.motion.x, e.motion.y };
+                    if ((SDL_PointInRect(&pt, &volume_rect)) && (e.motion.state & SDL_BUTTON_LMASK)) {
+                        const float fx = (float) (pt.x - volume_rect.x);
+                        volume_slider_value = (fx / ((float) volume_rect.w));   // a value between 0 and 1
+                        //printf("SLIDING! At %dx%d (%d volume_slider_value)\n", pt.x, pt.y, (int) SDL_round(volume_slider_value * 100.0f));
+                        //fflush(stdout);
                     }
                     break;
                 }
                 case SDL_DROPFILE: {
-                    if (audio_device) {
-                        SDL_CloseAudioDevice(audio_device);
-                        audio_device = 0;
-                    }
-
-                    SDL_FreeWAV(wavbuf);
-                    wavbuf = NULL;
-                    wavlen = 0;
-
-                    if (SDL_LoadWAV(e.drop.file, &wavspec, &wavbuf, &wavlen) == NULL) {
-                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file!", SDL_GetError(), window);
-                    } else {
-                    
-                        #if 0 // !!! FIXME: Come back to this.
-                        SDL_AudioSpec desired;
-                        SDL_zero(desired);
-                        desired.freq = 48000;
-                        desired.format = AUDIO_F32;
-                        desired.channels = 2;
-                        desired.samples = 1096;
-                        desired.callback = NULL;    
-                        #endif
-                    
-                        audio_device = SDL_OpenAudioDevice(NULL, 0, &wavspec, NULL, 0);
-                        if (audio_device == 0) {
-                            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't open audio device!", SDL_GetError(), window);
-                            SDL_FreeWAV(wavbuf);
-                            wavbuf = NULL;
-                            wavlen = 0;
-                        } else {
-                            SDL_QueueAudio(audio_device, wavbuf, wavlen);
-                            SDL_PauseAudioDevice(audio_device, paused);
-                        }
-                    }
-
+                    open_new_audio_file(e.drop.file);
                     SDL_free(e.drop.file);
                     break;
                 }
+            }
+        }
+
+        if (SDL_GetQueuedAudioSize(audio_device) < 8192) {
+            const int bytes_remaining = SDL_AudioStreamAvailable(stream);
+            if (bytes_remaining > 0) {
+                const int new_bytes = SDL_min(bytes_remaining, 32 * 1024);
+                static Uint8 converted_buffer[32 * 1024];
+                SDL_AudioStreamGet(stream, converted_buffer, new_bytes);    // !!! FIXME: error checking
+                const int num_samples = (new_bytes / sizeof (float));
+                float *samples = (float *) converted_buffer;
+                for (int i = 0; i < num_samples; i++) {
+                    samples[i] *= volume_slider_value;
+                }
+                SDL_QueueAudio(audio_device, converted_buffer, new_bytes);
             }
         }
 
@@ -126,6 +170,7 @@ int main(int argc, char **argv) {
 
         SDL_RenderFillRect(renderer, &rewind_rect);
         SDL_RenderFillRect(renderer, &pause_rect);
+        SDL_RenderFillRect(renderer, &volume_rect);
         SDL_RenderPresent(renderer);
 
         green = (green + 1) % 256;
